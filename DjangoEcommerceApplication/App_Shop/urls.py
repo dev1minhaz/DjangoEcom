3d7aa24d681bdaf791b15f6765d0a67fbb76c07c
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-01-10 01:09:19 +0600

    Product Model

--- DjangoEcommerceApplication/App_Shop/urls.py
+++ DjangoEcommerceApplication/App_Shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
+
 app_name="App_Shop"
 urlpatterns = [
-
+path('',views.Home.as_view(),name="home"),
 ]
--- DjangoEcommerceApplication/App_Shop/views.py
+++ DjangoEcommerceApplication/App_Shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+# import views
+from django.views.generic import ListView, DetailView
+
+# models
+from .models import Product
+
 # Create your views here.
+
+
+class Home(ListView):
+    model = Product
+    template_name = "App_Shop/home.html"
