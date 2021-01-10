3387ff8bc81a8f5958ef21be5825e66bb2f9cb9c
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-01-11 00:16:42 +0600

    Payment Gateway Part 2

--- DjangoEcommerceApplication/App_Payment/urls.py
+++ DjangoEcommerceApplication/App_Payment/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "App_Payment"
 urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
+    path('pay/', views.payment, name="payment"),
 ]
--- DjangoEcommerceApplication/App_Payment/views.py
+++ DjangoEcommerceApplication/App_Payment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect,redirect
 from django.contrib import messages
 
 
@@ -10,7 +10,11 @@ from .forms import BillingForm
 # login required
 from django.contrib.auth.decorators import login_required
 
-
+# for payment
+import requests
+from sslcommerz_python.payment import SSLCSession
+from decimal import Decimal
+import socket
 # Create your views here.
 @login_required
 def checkout(request):
@@ -25,4 +29,16 @@ def checkout(request):
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     order_items = order_qs[0].orderitems.all()
     order_total = order_qs[0].get_totals()
-    return render(request, 'App_Payment/checkout.html', context={'form': form, 'order_items': order_items, "order_total": order_total})
+    return render(request, 'App_Payment/checkout.html', context={'form': form, 'order_items': order_items, "order_total": order_total, "saved_address": saved_address})
+
+@login_required
+def payment(request):
+    saved_address = BillingAddress.objects.get_or_create(user=request.user)
+    if not saved_address[0].is_fully_filled():
+        messages.info(request,f"Please complete shipping address!")
+        return redirect("App_Payment:checkout")
+    
+    if not request.user.profile.is_fully_filled():
+        messages.info(request,f"Please complete profile details")
+        return redirect("App_Login:profile")
+    return render(request,"App_Payment/payment.html",context={})
