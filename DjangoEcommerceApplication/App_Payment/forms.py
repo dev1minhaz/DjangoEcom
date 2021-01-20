06461e52718c373eac8176915afefdd52560d6ed
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-01-20 12:33:26 +0600

    Normal State

--- DjangoEcommerceApplication/App_Payment/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.forms import fields
-from .models import BillingAddress
-class BillingForm(forms.ModelForm):
-    class Meta:
-        model=BillingAddress
-        fields=['address','zipcode','city','country']
--- DjangoEcommerceApplication/App_Payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "App_Payment"
-urlpatterns = [
-    path('checkout/', views.checkout, name="checkout"),
-    path('pay/', views.payment, name="payment"),
-    path('status/', views.complete, name="complete"),
-]
